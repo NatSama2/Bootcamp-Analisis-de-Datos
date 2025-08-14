89f084d198c8484d2ffc6680fde856ed6e8a15ad
Giselle Cifuentes Mankoch <124639827+NatSama2@users.noreply.github.com>
2025-08-14 19:39:19 -0400

    Add files via upload

---
+++ Modulo-7/Clase s06 SQL.sql
@@ -0,0 +1,91 @@
+
+
+--1. Crear una nueva tabla para registrar pagos de clientes
+
+--2. Insertar un nuevo pago
+
+--3. Agregar una columna de correo electrónico a Customers
+
+
+--4. Eliminar la columna recién creada
+
+
+--5. Eliminar la tabla de pagos
+
+--6. Listar todos los productos con precio mayor a 50
+;
+
+--7. Contar cuántos clientes hay por país
+
+
+--8. Obtener el total de pedidos por cliente
+
+--9. Listar productos y su categoría (INNER JOIN)
+
+
+--10. Mostrar todos los clientes y sus pedidos (LEFT JOIN)
+
+
+--11. Mostrar todos los pedidos y clientes (RIGHT JOIN)
+
+
+--12. Mostrar todos los clientes y pedidos (FULL OUTER JOIN)
+
+
+--13. Obtener el producto más caro
+
+
+--14. Calcular el total vendido por producto
+
+
+--15. Filtrar productos por categoría y precio
+
+--16. Obtener empleados que gestionaron más de 50 pedidos
+
+
+--17. Clientes sin pedidos
+
+--18. Pedidos realizados en 1997
+
+
+--19. Total de ingresos por año
+
+
+--20. Top 5 clientes con mayor gasto
+
+
+--21. Total vendido por categoría
+
+
+--22. Empleados con pedidos enviados por más de 2 transportistas
+
+
+--23. Productos sin pedidos
+
+
+--24. Precio promedio por categoría
+
+
+--25. Pedidos con más de 5 productos diferentes
+
+
+--26. Clientes que compraron productos de la categoría 'Beverages'
+
+--27. Transportistas y total de pedidos que enviaron
+
+
+--28. Productos y el número de clientes diferentes que lo compraron
+
+
+--29. Pedidos con valor total mayor a 1000
+
+
+--30. Clientes que hicieron pedidos en más de un año
+
+
+--31. Uso de funciones de ventana – Ranking de clientes por gasto total
+
+--32. Uso de CASE – Clasificación de productos por rango de precio
+
+
+--33. Promedio móvil de ventas por año-mes
